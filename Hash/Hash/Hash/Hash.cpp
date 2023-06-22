04a64fe3163a52823b258283a1cdee98839ca617
zonghoon Yi/zonghoon <whdgns0103@gmail.com>
2023-06-23 00:03:10 +0900

    ADD hash

---
+++ Hash/Hash/Hash/Hash.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+#define TABLE_SIZE	10
+
+struct Node {
+	int key;
+	int value;
+	Node* next;
+	Node(int a, int b) {
+		key = a;
+		value = b;
+		next = NULL;
+	}
+};
+
+Node* hash_table[TABLE_SIZE];
+
+int HashFunction(int key) {
+	return key & TABLE_SIZE;
+}
+
+void Add(int key, int value) {
+	Node* newNode = new Node(key, value);
+
+	int index = HashFunction(key);
+	if (hash_table[index] == NULL) {
+		hash_table[index] = newNode;
+	}
+	else {
+		Node* cur = hash_table[index];
+		while (cur != NULL) {
+			cur = cur->next;
+		}
+		newNode->next = hash_table[index];
+		hash_table[index] = newNode;
+	}
+	return;
+}
+
+int main() {
+
+}
